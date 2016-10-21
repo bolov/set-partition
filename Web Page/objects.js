4940847566f66a23c1258ad1ab139f6e8ab9af30
Mihail Bălan <mihail.balan@gmail.com>
2016-10-21 22:01:55 +0300

    Wip

--- Web Page/objects.js
+++ Web Page/objects.js
@@ -1,26 +1,30 @@
 class Item {
-	constructor(name, value)
-	{
-		this.name = name;
-		this.value = value;
-	}
+    constructor(name, value)
+    {
+        this.name = name;
+        this.value = value;
+    }
 }
 
 class Partition {
-	constructor(name, items)
-	{
-		this.name = name;
-		this.items = items;
-	}
+    constructor(name, items)
+    {
+        this.name = name;
+        this.items = items;
+    }
 }
 
 class Set {
-	constructor(name, partitions)
-	{
-		this.name = name;
-		this.partitions = partitions;
-	}
+    constructor(name, partitions)
+    {
+        this.name = name;
+        this.partitions = partitions;
+    }
 }
 
+//console.log(default_sets_hack_str);
 
-console.log(default_sets_hack_str);
+
+function clear_sets()
+{
+}
--- Web Page/yolo.js
+++ Web Page/yolo.js
@@ -1,45 +1,16 @@
 $(document).ready(function() {
-	$(".item").attr({"draggable": "true",
-					"ondragstart" : "on_drag_start(event)",
-					"ondragend" : "on_drag_end(event)",
-					});
-	
-	$(".partition").attr({"ondrop" : "on_drop(event)",
-					  "ondragover": "allow_drop(event)",	
-					  "ondragenter": "on_drag_enter(event)",
-	                  "ondragleave": "on_drag_leave(event)"});
+    $(".item").attr("draggable", "true");
+
+    $(".items").on({dragstart : on_drag_start, dragend : on_drag_end}, ".item");
+
+    $(".partitions")
+        .on({
+            dragover : allow_drop,
+            dragenter : on_drag_enter,
+            dragleave : on_drag_leave,
+        },
+            ".partition");
 });
-
-
-var default_sets = [
-	new Set("Default Set",
-		[new Partition("Remaining Items",
-			[new Item("Yolo", "250 GB"),
-			 new Item("Mai mult yolo", "600 GB"),
-			 new Item("Cel mai mult yolo", "1.5 TB"),
-			]
-		)]
-	),
-	new Set("Default Set",		 
-		[new Partition("C: 250 GB",
-			[new Item("Windows", "60 GB"),
-			 new Item("Programs", "200 GB"),
-			]
-		 ),
-		 new Partition("D: 500 GB",
-			[new Item("Kituri", "100 GB"),
-			 new Item("Games", "400 GB"),
-			]
-		 ),
-		]
-	),
-];
-
-
-console.log(default_sets);
-
-console.log(JSON.stringify(default_sets));
-
 
 function allow_drop(ev)
 {
@@ -52,47 +23,43 @@ var drag_over_counter = 0;
 function on_drag_start(ev)
 {
 
-	var target = $(ev.target).closest(".item");
-	
-	if (target.attr("id") == undefined || target.attr("id").length == 0)
-		target.attr("id", drag_tmp_id);
-	
-	ev.dataTransfer.setData("text", target.attr("id"));
-	console.log("set data: " + target.attr("id"));
+    var target = $(ev.target).closest(".item");
+
+    if (target.attr("id") == undefined || target.attr("id").length == 0)
+        target.attr("id", drag_tmp_id);
+
+    ev.dataTransfer.setData("text", target.attr("id"));
+    console.log("set data: " + target.attr("id"));
 }
 
 function on_drag_end(ev)
 {
-	$("#" + drag_tmp_id).removeAttr("id");
+    $("#" + drag_tmp_id).removeAttr("id");
 }
 
 function on_drop(ev)
 {
-	on_drag_leave(ev);
-	
-	items = $(ev.target).closest(".partition").children(".items");
-	var data = ev.dataTransfer.getData("text");
-	var source = $("#" + data);
-	items.append(source);
+    on_drag_leave(ev);
+
+    items = $(ev.target).closest(".partition").children(".items");
+    var data = ev.dataTransfer.getData("text");
+    var source = $("#" + data);
+    items.append(source);
 }
 
 function on_drag_enter(ev)
 {
-	if (drag_over_counter == 0) {
-		$(ev.target).closest(".partition").addClass("drop-zone");
-	}
-		
-	++drag_over_counter;
+    if (drag_over_counter == 0) {
+        $(ev.target).closest(".partition").addClass("drop-zone");
+    }
+
+    ++drag_over_counter;
 }
 
 function on_drag_leave(ev)
 {
-	--drag_over_counter;
-	if (drag_over_counter == 0) {
-		$(ev.target).closest(".partition").removeClass("drop-zone");
-	}	
+    --drag_over_counter;
+    if (drag_over_counter == 0) {
+        $(ev.target).closest(".partition").removeClass("drop-zone");
+    }
 }
-
-
-
-
